f065cca3d6bd700d25abb0ca14524dc2dd8bc4a5
Pedro Miguel <pmig.magalhaes@gmail.com>
2025-10-09 18:20:21 +0200

    Create Product details page, Cart page and Not Found page

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import ProductsList from '../features/products/ProductsList';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Our Products</h1>
+      <ProductsList />
+    </div>
+  );
+}
---
+++ src/app/product/[id]/page.jsx
@@ -0,0 +1,26 @@
+// src/app/product/[id]/page.jsx
+import { useParams } from 'next/navigation';
+import { useSelector, useDispatch } from 'react-redux';
+import { addToCart } from '../../../features/cart/cartSlice';
+
+export default function ProductDetails() {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const product = useSelector((state) =>
+    state.products.products.find((p) => p.id === parseInt(id))
+  );
+
+  if (!product) return <p>Product not found</p>;
+
+  return (
+    <div>
+      <h1>{product.title}</h1>
+      <img src={product.image} alt={product.title} width={200} />
+      <p>{product.description}</p>
+      <p>${product.price}</p>
+      <button onClick={() => dispatch(addToCart(product))}>
+        Add to Cart
+      </button>
+    </div>
+  );
+}
