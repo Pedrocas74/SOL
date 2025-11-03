0ac54199eaabe40a2a82147ce459426eda69888d
Pedro Miguel <pmig.magalhaes@gmail.com>
2025-11-03 10:06:36 +0100

    Style footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,23 @@
 
 import styles from "./styles/Footer.module.css";
 import Link from "next/link";
+import {
+  FaCcVisa,
+  FaCcMastercard,
+  FaCcAmex,
+  FaCcPaypal,
+  FaCcAmazonPay,
+  FaCcApplePay,
+  FaCcStripe,
+  FaGooglePay,
+} from "react-icons/fa";
+import Image from "next/image";
+// import githubIcon from "/images/icons/socials/github-mark-white.png";
+// import linkedinIcon from "/images/icons/socials/InBug-White.png";
 
 export default function Footer() {
   return (
     <section className={styles.bigContainer}>
-
       <div className={styles.promotionContainer}>
         <h5>10% DISCOUNT</h5>
         <h6>
@@ -15,46 +27,83 @@ export default function Footer() {
       </div>
       <hr />
 
-
       <div className={styles.policiesContainer}>
         <div className={styles.leftContainer}>
-          <p>&copy; 2025 SunLiT, Inc. All rights reserved.</p>
-          <Link href="/shipping-info">Shipping Info</Link>
-          <Link href="/terms-of-use">Terms of Use</Link>
-          <Link href="/privacy-policy">Privacy Policy</Link>
-          <Link href="/returns-and-refunds">Returns & Refunds</Link>
+          <p>
+            &copy; 2525 SunLiT, Inc. <br />
+            All rights reserved.
+          </p>
+          <div className={styles.legalLinks}>
+            <Link href="/shipping-info">Shipping Info</Link>
+            <Link href="/terms-of-use">Terms of Use</Link>
+            <Link href="/privacy-policy">Privacy Policy</Link>
+            <Link href="/returns-and-refunds">Returns & Refunds</Link>
+          </div>
           <a
-          href="https://fakestoreapi.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span>Powered by</span> FakeStore API
-        </a>
+            href="https://fakestoreapi.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by FakeStore API
+          </a>
         </div>
         <div className={styles.rightContainer}>
-            <a href="/" className="buttonPrimary">Contact</a>
-            <div className={styles.creditContainer}>
-                <p>CARDS</p>
-            </div>
+          <a href="/" className="buttonPrimary">
+            Contact
+          </a>
+          <div className={styles.creditContainer}>
+            <p>Methods of Payment:</p>
+            <FaCcVisa size={25} color="#333333" />
+            <FaCcMastercard size={25} color="#333333" />
+            <FaCcAmex size={25} color="#333333" />
+            <FaCcPaypal size={25} color="#333333" />
+            <FaCcAmazonPay size={25} color="#333333" />
+            <FaCcApplePay size={25} color="#333333" />
+            <FaCcStripe size={25} color="#333333" />
+            <FaGooglePay size={25} color="#333333" fill="#333333" />
+          </div>
         </div>
       </div>
 
-
-      <footer className={styles.footer}>
-        
-        <a
-          href="https://www.pedromagalhaes.vercel.app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Made by Pedro
-        </a>
-        <div className={styles.socials}>
-          <span>github</span>
-          <span>linkedin</span>
+      <footer className={styles.footer} id="footer">
+        <div className={styles.footerWrapper}>
+          <a
+            href="https://www.pedromagalhaes.vercel.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Made by Pedro
+          </a>
+          <div className={styles.socials}>
+            <a
+              href="https://github.com/Pedrocas74"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/images/icons/socials/github-mark-white.png"
+                width={25}
+                height={25}
+                priority
+                alt="Github link"
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/pedro-magalhães-1a3651334"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/images/icons/socials/InBug-White.png"
+                width={25}
+                height={25}
+                priority
+                alt="Linkedin link"
+              />
+            </a>
+          </div>
         </div>
       </footer>
-
     </section>
   );
 }
