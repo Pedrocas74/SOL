4436f763491390aff29cb58d242192ec0443dccd
Pedro Magalhães <pmig.magalhaes@gmail.com>
2025-11-14 15:26:09 +0100

    Remove unused folders/files

--- jest.setup.js
+++
@@ -1,7 +0,0 @@
-import '@testing-library/jest-dom';
-
-// Mock next/image if used
-jest.mock('next/image', () => (props) => {
-  // eslint-disable-next-line jsx-a11y/alt-text
-  return <img {...props} />;
-});
